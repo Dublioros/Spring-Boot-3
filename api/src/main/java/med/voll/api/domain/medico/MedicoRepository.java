fb49fd45ff509a06907b4f14ba3beea9a4d57049
Jose Carlo Gutierrez Schweitzer <129911770+Dublioros@users.noreply.github.com>
2023-09-16 11:14:16 -0500

    Stage 2 Proyecto Voll Api

--- api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -2,6 +2,7 @@ package med.voll.api.domain.medico;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,16 +12,16 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findByActivoTrue(@NotNull Long paginacion);
 
     @Query("""
-    SELECT m FROM Medico m
-    WHERE m.activo = 1 AND
-          m.especialidad = :especialidad AND
-          m.id NOT IN (
-              SELECT c.medico.id FROM Consulta c
-              WHERE c.fecha = :fecha
-          )
-    ORDER BY FUNCTION('rand')
-    LIMIT 1
-    """)
+            SELECT m FROM Medico m
+           WHERE m.activo = true AND
+                  m.especialidad = :especialidad AND
+                  m.id NOT IN (
+                      SELECT c.medico.id FROM Consulta c
+                      WHERE c.fecha = :fecha
+                  )
+            ORDER BY FUNCTION('rand')
+            LIMIT 1
+            """)
 
     Medico seleccionarMedicoConEspecialidadEnFecha(Especialidad especialidad, LocalDateTime fecha);
 
---
+++ api/src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,42 @@
+package med.voll.api.domain.medico;
+
+import med.voll.api.domain.paciente.Paciente;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+    @Autowired
+    private MedicoRepository medicoRepository;
+    @Autowired
+    private TestEntityManager em;
+    @Test
+    @DisplayName("Nulo cuando Medico esta en consulta con otro paciente en ese horario")
+
+    void seleccionarMedicoConEspecialidadEnFecha() {
+        var proximoLunes10H = LocalDate.now().
+                with(TemporalAdjusters.next(DayOfWeek.MONDAY)).
+                atTime(10,0);
+
+        var medicoLibre = medicoRepository.seleccionarMedicoConEspecialidadEnFecha(Especialidad.CARDIOLOGIA, proximoLunes10H);
+
+        assertThat(medicoLibre).isNull();
+    }
+}
